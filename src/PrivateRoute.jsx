ed06ee9c76a6c492ea49839b4d6c1be1ab0c1729
stivenrodriguezm <stivenrodriguezm@outlook.com>
2025-03-23 15:24:47 -0500

    Updates

--- src/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ component: Component }) => {
-  const token = localStorage.getItem("accessToken");
-  return token ? <Component /> : <Navigate to="/ordenesPedidoSW/login" />;
-};
-
-
-export default PrivateRoute;
--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function PrivateRoute({ children }) {
-  const accessToken = localStorage.getItem("accessToken");
-  return accessToken ? children : <Navigate to="/login" />;
-}
-
-export default PrivateRoute;
