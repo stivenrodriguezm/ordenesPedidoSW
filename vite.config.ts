a3bdd886f29d8c3bf8cc376fd3b33946251ae3f4
stivenrodriguezm <stivenrodriguezm@outlook.com>
2025-03-23 14:54:22 -0500

    v20k

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ordenesPedidoSW/", // Agrega el nombre del repositorio
+  base: "/ordenesPedidoSW/",
+  build: {
+    outDir: "build",
+  },
+  server: {
+    historyApiFallback: true, // Asegura que las rutas sean manejadas por React
+  },
 });
