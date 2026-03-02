c6268a728ef8f60f23706994208c25aac69bd7e2
stivenrodriguezm <stivenrodriguezm@outlook.com>
2026-03-02 15:38:09 -0500

    telas

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://localhost:8000/api'; // URL para producción
+const API_BASE_URL = 'https://api.muebleslottus.com/api'; // URL para producción
 
 export default API_BASE_URL;
