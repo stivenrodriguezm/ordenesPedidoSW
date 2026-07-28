35b781a8a7d27e0ee08710201e4c43e7cb4dff06
Stiven <stiven@Stivens-MacBook-Air.local>
2026-07-28 15:27:49 -0500

    Fix API URL for production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,12 @@
-// Configuración de la URL base del API - ENTORNO DE DESARROLLO LOCAL
-const API_BASE_URL = 'http://localhost:8000/api';
+// Configuración de la URL base del API - ENTORNO DE DESARROLLO Y PRODUCCION
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+const API_BASE_URL = isLocalhost ? 'http://localhost:8000/api' : 'https://api.muebleslottus.com/api';
 
 export default API_BASE_URL;
